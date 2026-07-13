da34be00536e898953cef52fc205903df6b1be8d
Lauren Lee <laurenwyl@gmail.com>
2026-07-13 14:41:35 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
         <div aria-hidden="true">
           <TerminalText>
             {`Hello, my name is Lauren.
-            I am a junior at Yale pursuing a combined bachelors and masters in Computer Science with certificates in Chinese Language and Ethnography.
+            I am a junior at Yale pursuing a combined bachelors and masters in Computer Science with certificates in Chinese Language and Ethnography. I am interested in Software Engineering and Product Management roles.
             If you'd like to learn more about me, select an option ( Use mouse or ↑ / ↓ to navigate. Click of press Enter to select):
               A. About
               B. Courses
